b1fc0767dfa9d3aff5ae8e668635e07182db78b8
Teo Podobnik <teo.podobnik1234@gmail.com>
2026-02-13 16:34:09 +0000

    comment bpf_for helper

--- loops.c
+++ loops.c
@@ -88,37 +88,24 @@ extern int bpf_iter_num_new(struct bpf_iter_num *it, int start,
                             int end) __weak __ksym;
 extern int *bpf_iter_num_next(struct bpf_iter_num *it) __weak __ksym;
 extern void bpf_iter_num_destroy(struct bpf_iter_num *it) __weak __ksym;
-#ifndef bpf_for
-/* bpf_for(i, start, end) implements a for()-like looping construct that sets
- * provided integer variable *i* to values starting from *start* through,
- * but not including, *end*. It also proves to BPF verifier that *i* belongs
- * to range [start, end), so this can be used for accessing arrays without
- * extra checks.
- *
- * Note: *start* and *end* are assumed to be expressions with no side effects
- * and whose values do not change throughout bpf_for() loop execution. They do
- * not have to be statically known or constant, though.
- *
- * Note: similarly to bpf_for_each(), it relies on C99 feature of declaring
- * for() loop bound variables and cleanup attribute, supported by GCC and Clang.
- */
-#define bpf_for(i, start, end)                                                                                                                                                                                          \
-  for (/* initialize and define destructor */                                                                                                                                                                           \
-       struct bpf_iter_num ___it __attribute__((                                                                                                                                                                        \
-           aligned(8),                      /* enforce, just in case */                                                                                                                                                 \
-           cleanup(bpf_iter_num_destroy))), /* ___p pointer is necessary to                                                                                                                                             \
-                                               call bpf_iter_num_new() *once*                                                                                                                                           \
-                                               to init ___it */                                                                                                                                                         \
-       *___p                                                                                                                                                                                                            \
-       __attribute__((unused)) = (bpf_iter_num_new(&___it, (start), (end)), /* this is a workaround for Clang bug: it currently doesn't emit BTF */ /* for bpf_iter_num_destroy() when used from cleanup() attribute */ \
-                                  (void)bpf_iter_num_destroy, (void *)0);                                                                                                                                               \
-       ({                                                                                                                                                                                                               \
-         /* iteration step */                                                                                                                                                                                           \
-         int *___t = bpf_iter_num_next(&___it);                                                                                                                                                                         \
-         /* termination and bounds check */                                                                                                                                                                             \
-         (___t && ((i) = *___t, (i) >= (start) && (i) < (end)));                                                                                                                                                        \
-       });)
-#endif /* bpf_for */
+//#define bpf_for(i, start, end)                                                                  
+//  for (                                                                                         
+//       struct bpf_iter_num ___it __attribute__((                                                
+//           aligned(8),              
+//           cleanup(bpf_iter_num_destroy))),                                                     
+//                                                                                                
+//       *___p __attribute__((unused)) = (                                                        
+//           bpf_iter_num_new(&___it, (start), (end)),                                            
+//           (void)bpf_iter_num_destroy,                                                          
+//           (void *)0);                                                                          
+//                                                                                                
+//       ({                                                                                       
+//         // Advance the iterator and get the next value
+//         int *___t = bpf_iter_num_next(&___it);                                                
+//         (___t && ((i) = *___t, (i) >= (start) && (i) < (end)));                                
+//       })
+//  ;)
+//#endif // bpf_for
 
 SEC("xdp")
 int xdp_prog_bpf_for_helper(struct xdp_md *ctx) {
